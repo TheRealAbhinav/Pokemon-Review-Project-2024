2d2114c3d5a36cd48fa3b8e192be28f898824736
Abhinav Tyagi <Abhinav7728@gmail.com>
2024-01-27 00:49:41 +0530

    Add post mapping to add an pokemon, Also basic refactor.

--- api/src/main/java/com/pokemonReview/api/controllers/PokemonController.java
+++ api/src/main/java/com/pokemonReview/api/controllers/PokemonController.java
@@ -1,12 +1,12 @@
 package com.pokemonReview.api.controllers;
 
 import com.pokemonReview.api.models.Pokemon;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,19 +14,23 @@ import java.util.Optional;
 @RequestMapping("/api")
 public class PokemonController {
 
-    private List<Pokemon> pokemons = List.of(
+    // Use ArrayList instead of List because at Post mapping we are trying to dynamically add an element inside the list
+    // Since List are immutable collection, we can not do that at runtime. It will throw an error.
+    private ArrayList<Pokemon> pokemons = new ArrayList<>(Arrays.asList(
             new Pokemon(1,"Pikachu","Electric"),
             new Pokemon(2,"Squirtle","Water"),
             new Pokemon(3,"Charmander","Fire"),
             new Pokemon(4,"Balbasaur","Leaf")
-    );
+    ));
 
-    @GetMapping("pokemons") // http://localhost:8080/api/pokemons - Will return all pokemons
+    // http://localhost:8080/api/pokemons - Will return all pokemons
+    @GetMapping("pokemons")
     public ResponseEntity<List<Pokemon>> getPokemons(){
         return ResponseEntity.ok(pokemons);
     }
 
-    @GetMapping("pokemon/{id}") // http://localhost:8080/api/pokemon/1 - Will return all pokemon of id 1
+    // http://localhost:8080/api/pokemon/1 - Will return all pokemon of id 1
+    @GetMapping("pokemon/{id}")
     public ResponseEntity<Pokemon> getPokemon(@PathVariable int id){
         Optional<Pokemon> pokemon = pokemons.stream().filter(P -> P.getId() == id).findFirst();
         return pokemon.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.badRequest().build());
@@ -36,6 +40,19 @@ public class PokemonController {
 //        else {
 //            return ResponseEntity.badRequest().build();
 //        }
+    }
+
+    // http://localhost:8080/api/pokemon - Will take a Pokemon JSON object and add it to DB and return that pokemon in POJO with its id.
+    @PostMapping("pokemon")
+    public ResponseEntity<Pokemon> addPokemon(@RequestBody Pokemon newPokemon){
+        newPokemon.setId(pokemons.size()+1); // default id would be 0, change that.
+        if(pokemons.add(newPokemon)){
+            return ResponseEntity.ok(pokemons.get(pokemons.size()-1));
+        }
+        else {
+            System.out.println("<<<< "+pokemons.get(pokemons.size()-1));
+            return ResponseEntity.badRequest().build();
+        }
     }
 
 }
