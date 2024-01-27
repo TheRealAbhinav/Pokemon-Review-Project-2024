865df82659215ed2ddc3a7e2d818e64288acabf4
Abhinav Tyagi <Abhinav7728@gmail.com>
2024-01-27 20:40:40 +0530

    Code Refactore - Imports Optimizations

--- api/src/main/java/com/pokemonReview/api/controllers/PokemonController.java
+++ api/src/main/java/com/pokemonReview/api/controllers/PokemonController.java
@@ -1,7 +1,6 @@
 package com.pokemonReview.api.controllers;
 
 import com.pokemonReview.api.models.Pokemon;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
