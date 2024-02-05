21889f5226083cdd1ebe4fc637d9ec1fe6105e2a
Abhinav Tyagi <Abhinav7728@gmail.com>
2024-02-05 18:12:47 +0530

    Code Refactore - Fix indentations

--- api/src/main/java/com/pokemonReview/api/controllers/PokemonController.java
+++ api/src/main/java/com/pokemonReview/api/controllers/PokemonController.java
@@ -27,84 +27,50 @@ public class PokemonController {
     }
 
     // http://localhost:8080/api/pokemons - Will return all pokemons
-    @Operation(
-            summary = "Fetch all pokemons",
-            description = "Fetch all pokemons present in the DB"
-    ) // Swagger - To give header for individual API
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "All Pokemons fetched successfully!"),
-            @ApiResponse(responseCode = "404", description = "No pokemon present in the DB"),
-    }) // Swagger - To give better message for each response from the API
+    // Swagger - To give header for individual API
+    @Operation(summary = "Fetch all pokemons", description = "Fetch all pokemons present in the DB")
+    // Swagger - To give better message for each response from the API
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "All Pokemons fetched successfully!"), @ApiResponse(responseCode = "404", description = "No pokemon present in the DB"),})
     @GetMapping("pokemons")
     public ResponseEntity<List<Pokemon>> getPokemons() {
         return ResponseEntity.ok(service.getAllPokemons());
     }
 
     // http://localhost:8080/api/pokemons/page?pageNo=0&pageSize=5 - Will return all pokemons in Pagination format. each page will show 5 records, and we are requesting first page
-    @Operation(
-            summary = "Fetch all pokemons",
-            description = "Fetch all pokemons present in the DB, And return the result in pagination protocol defined at parameters"
-    ) // Swagger - To give header for individual API
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Pokemons fetched successfully!"),
-            @ApiResponse(responseCode = "404", description = "No pokemon present in the DB"),
-    }) // Swagger - To give better message for each response from the API
+    @Operation(summary = "Fetch all pokemons", description = "Fetch all pokemons present in the DB, And return the result in pagination protocol defined at parameters")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Pokemons fetched successfully!"), @ApiResponse(responseCode = "404", description = "No pokemon present in the DB"),})
     @GetMapping("pokemons/page")
     public ResponseEntity<PokemonPageResponse> getPokemons(@RequestParam(value = "pageNo", defaultValue = "0", required = false) int pageNo, @RequestParam(value = "pageSize", defaultValue = "10", required = false) int pageSize) {
         return ResponseEntity.ok(service.getAllPokemons(pageNo, pageSize));
     }
 
     // http://localhost:8080/api/pokemon/1 - Will return pokemon of id 1
-    @Operation(
-            summary = "Fetch a single pokemon",
-            description = "Fetch the pokemon from the DB using ID given in the URL"
-    ) // Swagger - To give header for individual API
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Pokemon fetched successfully!"),
-            @ApiResponse(responseCode = "404", description = "Pokemon not found, Try with different ID"),
-    }) // Swagger - To give better message for each response from the API
+    @Operation(summary = "Fetch a single pokemon", description = "Fetch the pokemon from the DB using ID given in the URL")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Pokemon fetched successfully!"), @ApiResponse(responseCode = "404", description = "Pokemon not found, Try with different ID"),})
     @GetMapping("pokemon/{id}")
     public ResponseEntity<Pokemon> getPokemon(@PathVariable int id) {
         return new ResponseEntity<>(service.findPokemonById(id), HttpStatus.OK);
     }
 
     // http://localhost:8080/api/pokemon - Will take a Pokemon JSON object and add it to DB and return that pokemon in POJO with its id.
-    @Operation(
-            summary = "Save pokemon",
-            description = "Save a single pokemon in the DB"
-    ) // Swagger - To give header for individual API
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "Pokemon saved successfully!"),
-            @ApiResponse(responseCode = "404", description = "Pokemon was not saved. Check JSON."),
-    }) // Swagger - To give better message for each response from the API
+    @Operation(summary = "Save pokemon", description = "Save a single pokemon in the DB")
+    @ApiResponses(value = {@ApiResponse(responseCode = "201", description = "Pokemon saved successfully!"), @ApiResponse(responseCode = "404", description = "Pokemon was not saved. Check JSON."),})
     @PostMapping("pokemon")
     public ResponseEntity<Pokemon> addPokemon(@RequestBody PokemonDto newPokemonDto) {
         return new ResponseEntity<>(service.savePokemon(newPokemonDto), HttpStatus.CREATED);
     }
 
     // http://localhost:8080/api/pokemon/1 - Will update the pokemon with id 1 with the given update
-    @Operation(
-            summary = "Update pokemon",
-            description = "Update a single pokemon with ID given at URL path"
-    ) // Swagger - To give header for individual API
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "Pokemon updated successfully!"),
-            @ApiResponse(responseCode = "404", description = "Pokemon update failed"),
-    }) // Swagger - To give better message for each response from the API
+    @Operation(summary = "Update pokemon", description = "Update a single pokemon with ID given at URL path")
+    @ApiResponses(value = {@ApiResponse(responseCode = "201", description = "Pokemon updated successfully!"), @ApiResponse(responseCode = "404", description = "Pokemon update failed"),})
     @PutMapping("pokemon/{id}")
     public ResponseEntity<Pokemon> updatePokemon(@RequestBody PokemonDto updateDto, @PathVariable int id) {
         return new ResponseEntity<>(service.updatePokemon(updateDto, id), HttpStatus.CREATED);
     }
 
     // http://localhost:8080/api/pokemon/1 - Will remove the pokemon with id 1
-    @Operation(
-            summary = "Delete pokemon",
-            description = "Remove a single pokemon from DB with ID given at URL path"
-    ) // Swagger - To give header for individual API
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "pokemon deleted successfully!"),
-            @ApiResponse(responseCode = "404", description = "Pokemon was not deleted from DB"),
-    }) // Swagger - To give better message for each response from the API
+    @Operation(summary = "Delete pokemon", description = "Remove a single pokemon from DB with ID given at URL path")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "pokemon deleted successfully!"), @ApiResponse(responseCode = "404", description = "Pokemon was not deleted from DB"),})
     @DeleteMapping("pokemon/{id}")
     public ResponseEntity<String> removePokemon(@PathVariable int id) {
         return ResponseEntity.ok("Deleted Pokemon :: " + service.removePokemonById(id));
--- api/src/main/java/com/pokemonReview/api/controllers/ReviewsController.java
+++ api/src/main/java/com/pokemonReview/api/controllers/ReviewsController.java
@@ -16,7 +16,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-@Tag(name = "Reviews", description = "Pokemon Reviews CRUD service APIs") // To edit the Swagger API heading
+@Tag(name = "Reviews", description = "Pokemon Reviews CRUD service APIs")
 public class ReviewsController {
     private ReviewsService reviewsService;
 
@@ -26,42 +26,24 @@ public class ReviewsController {
     }
 
     // http://localhost:8080/api/pokemons/1/review - Will create a new review for pokemon with id 1
-    @Operation(
-            summary = "Create review",
-            description = "Create a single new review for pokemon with ID given at URL path"
-    ) // Swagger - To give header for individual API
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "Review added for the pokemon successfully!"),
-            @ApiResponse(responseCode = "404", description = "Review was not created"),
-    }) // Swagger - To give better message for each response from the API
+    @Operation(summary = "Create review", description = "Create a single new review for pokemon with ID given at URL path")
+    @ApiResponses(value = {@ApiResponse(responseCode = "201", description = "Review added for the pokemon successfully!"), @ApiResponse(responseCode = "404", description = "Review was not created"),})
     @PostMapping("pokemons/{pokemonId}/review")
     public ResponseEntity<Reviews> createReview(@PathVariable int pokemonId, @RequestBody ReviewsDto reviewsDto) {
         return new ResponseEntity<>(reviewsService.createReview(pokemonId, reviewsDto), HttpStatus.CREATED);
     }
 
     // http://localhost:8080/api/pokemons/1/reviews - Will return all the reviews of pokemon with id 1
-    @Operation(
-            summary = "Fetch review",
-            description = "Fetch a single or multiple reviews for pokemon with ID present at the URL path"
-    ) // Swagger - To give header for individual API
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "All the reviews fetched for the pokemon successfully!"),
-            @ApiResponse(responseCode = "404", description = "Reviews not present"),
-    }) // Swagger - To give better message for each response from the API
+    @Operation(summary = "Fetch review", description = "Fetch a single or multiple reviews for pokemon with ID present at the URL path")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "All the reviews fetched for the pokemon successfully!"), @ApiResponse(responseCode = "404", description = "Reviews not present"),})
     @GetMapping("pokemons/{pokemonId}/reviews")
     public ResponseEntity<List<Reviews>> getPokemonReviewsByPokemonId(@PathVariable int pokemonId) {
         return new ResponseEntity<>(reviewsService.getAllPokemonReviewsByPokemonId(pokemonId), HttpStatus.OK);
     }
 
     // http://localhost:8080/api/pokemons/reviews/1 - Will return the review with id 1
-    @Operation(
-            summary = "Fetch single review",
-            description = "Fetch a single review from the DB with ID present at the URL path"
-    ) // Swagger - To give header for individual API
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Review added for the pokemon successfully!"),
-            @ApiResponse(responseCode = "404", description = "Review was not created"),
-    }) // Swagger - To give better message for each response from the API
+    @Operation(summary = "Fetch single review", description = "Fetch a single review from the DB with ID present at the URL path")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Review added for the pokemon successfully!"), @ApiResponse(responseCode = "404", description = "Review was not created"),})
     @GetMapping("pokemons/reviews/{reviewId}")
     public ResponseEntity<Reviews> getReviewById(@PathVariable int reviewId) {
         return new ResponseEntity<>(reviewsService.getReviewById(reviewId), HttpStatus.OK);
@@ -69,42 +51,24 @@ public class ReviewsController {
 
     // http://localhost:8080/api/pokemons/1/reviews/2 - Will return the review with id 2 of pokemon with id 1
     // Each pokemon can have multiple reviews, using this API we can select individual review of a single pokemon
-    @Operation(
-            summary = "Fetch single review for a pokemon",
-            description = "Will fetch a single review for a single pokemon"
-    ) // Swagger - To give header for individual API
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Review fetched successfully!"),
-            @ApiResponse(responseCode = "404", description = "Review was not fetched"),
-    }) // Swagger - To give better message for each response from the API
+    @Operation(summary = "Fetch single review for a pokemon", description = "Will fetch a single review for a single pokemon")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Review fetched successfully!"), @ApiResponse(responseCode = "404", description = "Review was not fetched"),})
     @GetMapping("pokemons/{pokemonId}/reviews/{reviewId}")
     public ResponseEntity<Reviews> getPokemonReviewByReviewId(@PathVariable int pokemonId, @PathVariable int reviewId) {
         return new ResponseEntity<>(reviewsService.getPokemonReviewByReviewId(pokemonId, reviewId), HttpStatus.OK);
     }
 
-    @Operation(
-            summary = "Update a single review for a pokemon",
-            description = "Will update a single review for a single pokemon"
-    ) // Swagger - To give header for individual API
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "Review updated successfully!"),
-            @ApiResponse(responseCode = "404", description = "Review was not updated"),
-    }) // Swagger - To give better message for each response from the API
     // http://localhost:8080/api/pokemons/1/reviews/2 - Will perform an update on review of id 2 in pokemon with id 1
+    @Operation(summary = "Update a single review for a pokemon", description = "Will update a single review for a single pokemon")
+    @ApiResponses(value = {@ApiResponse(responseCode = "201", description = "Review updated successfully!"), @ApiResponse(responseCode = "404", description = "Review was not updated"),})
     @PutMapping("pokemons/{pokemonId}/reviews/{reviewId}")
     public ResponseEntity<Reviews> updatePokemonReview(@PathVariable int pokemonId, @PathVariable int reviewId, @RequestBody ReviewsDto reviewsDto) {
         return new ResponseEntity<>(reviewsService.updatePokemonReview(pokemonId, reviewId, reviewsDto), HttpStatus.ACCEPTED);
     }
 
     // http://localhost:8080/api/pokemons/1/reviews/2 - Will remove review of id 2 in pokemon with id 1
-    @Operation(
-            summary = "Delete a single review for a pokemon",
-            description = "Will delete a single review for a single pokemon"
-    ) // Swagger - To give header for individual API
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Review deleted successfully!"),
-            @ApiResponse(responseCode = "404", description = "Review was not removed"),
-    }) // Swagger - To give better message for each response from the API
+    @Operation(summary = "Delete a single review for a pokemon", description = "Will delete a single review for a single pokemon")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Review deleted successfully!"), @ApiResponse(responseCode = "404", description = "Review was not removed"),})
     @DeleteMapping("pokemons/{pokemonId}/reviews/{reviewId}")
     public ResponseEntity<String> deletePokemonReviewById(@PathVariable int pokemonId, @PathVariable int reviewId) {
         return ResponseEntity.ok("Review Deleted :: " + reviewsService.deletePokemonReview(pokemonId, reviewId));
